800005d7bb18900ea4b693c34374437392ef94e7
TheCrispiestToast <61917048+TheCrispiestToast@users.noreply.github.com>
2020-10-06 13:03:53 -0400

    Cleaned Up Code

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import logging
 from xml.etree import ElementTree
 import aiohttp
 import asyncio
-import json
 
 # TODO: Create File to Store Previously Asked Questions and Pull from them Instead of Constantly Accessing Wolfram API (and taking up all my fuckin queries)
 
@@ -23,13 +22,14 @@ client = discord.Client()
 discordToken = envariables.discordToken
 wolframToken = envariables.wolframToken
 
+
 # Informs of Bot Connection Information on Ready
 @client.event
 async def on_ready():
-    print('Logged in as {}'.format(client.user))
+    print(f'Logged in as {client.user}')
     await client.change_presence(activity=discord.Game(name='"Q? help"'))
     await client.get_channel(762093041116381198).send('Bot Enabled.')
-    print("In {} servers.".format(len(client.guilds)))
+    print(f'In {len(client.guilds)} servers.')
     for server in client.guilds:
         print(server.name)
 
@@ -45,68 +45,55 @@ async def on_message(message):
     # Only Parses Message If Proceeded with Prefix
     if message.content.startswith('Q? '):
         messageContent = message.content[3:]
+        title = f'{str(message.author)} asked, "{messageContent}"'
         answer = await message.channel.send(
-            embed=discord.Embed(title=str(message.author) + ' asked, "{}"'.format(messageContent),
-                                description="Answering...", color=0xffff00))
-        
+            embed=discord.Embed(title=title,
+                                description="Answering...",
+                                color=0xffff00))
+
         # Predefined Responses
         if 'who am i' in messageContent.lower():
-            await answer.edit(
-                embed=discord.Embed(title=str(message.author) + ' asked, "{}"'.format(messageContent),
-                                    description='You are {}.'.format(str(message.author)),
-                                    color=0x00ff00))
+            description = f'You are {str(message.author)}.'
+            color = 0x00ff00
 
-        elif 'who are you' in messageContent.lower():
-            await answer.edit(
-                embed=discord.Embed(title=str(message.author) + ' asked, "{}"'.format(messageContent),
-                                    description='I am {}.'.format(str(client.user)),
-                                    color=0x00ff00))
-            
-        elif 'your name' in messageContent.lower():
-            await answer.edit(
-                embed=discord.Embed(title=str(message.author) + ' asked, "{}"'.format(messageContent),
-                                    description='I am {}.'.format(str(client.user)),
-                                    color=0x00ff00))
+        elif 'who are you' in messageContent.lower() or 'your name' in messageContent.lower():
+            description = f'I am {str(client.user)}.'
+            color = 0x00ff00
 
         elif 'where am i' in messageContent.lower():
-            await answer.edit(
-                embed=discord.Embed(title=str(message.author) + ' asked, "{}"'.format(messageContent),
-                                    description='You are in the #{} channel on {}.'.format(message.channel, message.guild),
-                                    color=0x00ff00))
+            description = f'You are in the #{message.channel} channel on {message.guild}.'
+            color = 0x00ff00
 
         elif messageContent.lower().startswith('help'):
-            await answer.edit(
-                embed=discord.Embed(title=str(message.author) + ' asked, "{}"'.format(messageContent),
-                                    description='Ask me ANY question and I\'ll do my best to answer it!',
-                                    color=0x00ff00))
+            description = 'Ask me any question and I\'ll do my best to answer it!'
+            color = 0x00ff00
 
         # Sends Message to Wolfram API and Returns Result as Embed
         else:
             try:
                 async with aiohttp.ClientSession() as session:
-                    async with session.get(
-                            'https://api.wolframalpha.com/v2/query?input={}&appid={}'.format(
-                                    messageContent.replace('+', 'plus'), wolframToken)) as response:
+                    async with session.get(f'https://api.wolframalpha.com/v2/query?input={messageContent.replace("+", "plus")}&appid={wolframToken}') as response:
                         # TODO: Change WolframAlpha Output Data Type from XML to JSON
                         tree = ElementTree.fromstring(await response.content.read())
                         # ? Should I Keep the Special Orange Empty Message Embed or Defaut to the Red One?
                         if tree[1][0].find('plaintext').text == None or tree[1][0].find('plaintext').text == '':
-                            await answer.edit(
-                                embed=discord.Embed(title=str(message.author) + ' asked, "{}"'.format(messageContent),
-                                                    description='Answer is Empty or Missing...',
-                                                    color=0xff8000))
+                            description = 'Answer is Empty or Missing...'
+                            color = 0xff8000
+
                         else:
-                            await answer.edit(
-                                embed=discord.Embed(title=str(message.author) + ' asked, "{}"'.format(messageContent),
-                                                    description=tree[1][0].find('plaintext').text,
-                                                    color=0x00ff00))
+                            description = tree[1][0].find('plaintext').text
+                            color = 0x00ff00
+
 
             # Returns if Answer Cannot be Found
             except IndexError:
-                await answer.edit(
-                    embed=discord.Embed(title=str(message.author) + ' asked, "{}"'.format(messageContent),
-                                        description='Unable to Answer Question',
-                                        color=0xff0000))
+                description = 'Unable to Answer Question'
+                color = 0xff0000
+
+        await answer.edit(
+                    embed=discord.Embed(title=title,
+                                        description=description,
+                                        color=color))
 
 
 # Runs Bot... Obviously
