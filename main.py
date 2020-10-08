f949f3e21d196fff198a8baae5be79d33e1485e6
TheCrispiestToast <61917048+TheCrispiestToast@users.noreply.github.com>
2020-10-08 18:17:13 -0400

    Admin Presence Command Added

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ from random import choice
 # Logs Discord DEBUG Logs to discord.log
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler = logging.FileHandler(
+    filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
 # Accesses Discord Client
@@ -44,48 +46,42 @@ async def on_message(message):
         return
 
     # Only Parses Message If Proceeded with Prefix
-    if message.content.startswith('Q? '):     
+    if message.content.startswith('Q? '):
         messageContent = message.content[3:]
-                                    
+
         # Regular Commands
         title = f'{str(message.author)} asked, "{messageContent}"'
         answer = await message.channel.send(
             embed=discord.Embed(title=title,
                                 description="Answering...",
-                                color=0xffff00)) # Yellow
-        color = 0x00ff00 # Green
-        
+                                color=0xffff00))  # Yellow
+        color = 0x00ff00  # Green
+
         # Admin Commands
         # All Admin Commands Require You to Define a "command" and "description" Variable for Embed
         if messageContent in adminCommands:
             if message.author.id == 330116875755323393:
-                if messageContent in ['admin','admin help']:
+                if messageContent in ['admin', 'admin help']:
                     command = 'Help'
-                    description = "Q? admin servers - Lists Servers Bot is In"
-                    
+                    description = "Q? admin servers - Lists Servers Bot is In\nQ? admin change_presence <input> - Changes Rich Presence of Bot"
+
                 elif messageContent == 'admin servers':
                     command = 'Servers'
                     description = f'In {len(client.guilds)} servers:'
                     for server in client.guilds:
                         description += f'\n{server.name}'
-            
+                        
+                elif messageContent == 'admin change_presence':
+                    command = 'Change Presence'
+                    description = f'Changing Presence to "{messageContent[22:]}"...'
+                    await client.change_presence(activity=discord.Game(name=messageContent[22:]))
+
             title = f'Admin Command "**{command}**" Triggered'
-            color = 0xb500b5 # Purple
-        
-        # TODO: Replace (some) Predefined Responses with Answer Responses
-        # Predefined Responses
-        # // elif 'who am i' in messageContent.lower():
-        # //     description = f'You are {str(message.author)}.'
+            color = 0xb500b5  # Purple
 
-        # // elif 'who are you' in messageContent.lower() or 'your name' in messageContent.lower():
-        # //     description = f'I am {str(client.user)}.'
-
-        # // elif 'where am i' in messageContent.lower():
-        # //     description = f'You are in the #{message.channel} channel on {message.guild}.'
-
-        # // elif messageContent.lower().startswith('help'):
-        # //     description = 'Ask me any question and I\'ll do my best to answer it!'
-        
+        # Predefined Question Responses
+        elif messageContent.lower().startswith('help'):
+            description = 'Ask me any question and I\'ll do my best to answer it!'
 
         # Sends Message to Wolfram API and Returns Result as Embed
         else:
@@ -95,23 +91,28 @@ async def on_message(message):
                         # TODO: Change WolframAlpha Output Data Type from XML to JSON
                         tree = ElementTree.fromstring(await response.content.read())
                         # ? Should I Keep the Special Orange Empty Message Embed or Defaut to the Red One?
-                        if tree[1][0].find('plaintext').text == None or tree[1][0].find('plaintext').text == '':
-                            description = 'Answer is Empty or Missing...'
-                            color = 0xff8000 # Orange
+                        if tree[1][0].find('plaintext').text not in [None, '']:
+                            # Predefined Answer Responses
+                            if tree[1][0].find('plaintext').text == 'My name is Wolfram|Alpha.':
+                                description = f'My name is {client.user}'
+                            else:
+                                description = tree[1][0].find('plaintext').text
 
                         else:
-                            description = tree[1][0].find('plaintext').text
+                            # Empty or Missing Answer
+                            description = '¯\_(ツ)_/¯'
+                            color = 0xff8000  # Orange
 
             # Returns if Answer Cannot be Found
             except IndexError:
-                description = 'Unable to Answer Question'
-                color = 0xff0000 # Red
+                description = '¯\_(ツ)_/¯'
+                color = 0xff0000  # Red
 
         # Applies title, description, and color Changes
         await answer.edit(
-                    embed=discord.Embed(title=title,
-                                        description=description,
-                                        color=color))
+            embed=discord.Embed(title=title,
+                                description=description,
+                                color=color))
 
 
 # Runs Bot... Obviously
